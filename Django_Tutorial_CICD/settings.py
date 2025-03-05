1df5a690aa34e6d7c9e73cad4c6ef91402c3fd24
Apurv Patel <patelapurv2001@gmail.com>
2025-03-05 21:09:27 +1300

    updated the vercel file

--- Django_Tutorial_CICD/settings.py
+++ Django_Tutorial_CICD/settings.py
@@ -70,7 +70,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "Django_Tutorial_CICD.wsgi.application"
+# WSGI_APPLICATION = "Django_Tutorial_CICD.wsgi.application"
+WSGI_APPLICATION = "Django_Tutorial_CICD.wsgi.app"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
